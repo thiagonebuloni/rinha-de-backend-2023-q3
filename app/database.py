29f6839016e4352c1f520fea937468388e1d876d
thiagonebuloni <thiago.nebuloni@gmail.com>
2023-08-14 08:30:38 -0300

    :bug: recreated venv, working again

--- app/database.py
+++ app/database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:767130@localhost/rinha-de-backend-2023-q3"
-)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
